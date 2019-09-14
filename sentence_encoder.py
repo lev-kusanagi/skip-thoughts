c4f11318ba64a4a15f5d0d0f6e4a1f93ef480f08
Lev Kusanagi <levkusanagi@gmail.com>
2019-09-14 12:47:45 +0200

    produce npys and save every separate file every 80K vectors

--- sentence_encoder.py
+++ sentence_encoder.py
@@ -1,47 +1,45 @@
 import skipthoughts
-from nltk import tokenize
 import unicodecsv
-import numpy
+import numpy as np
 from tqdm import tqdm
 
 model = skipthoughts.load_model()
 encoder = skipthoughts.Encoder(model)
+embedding_size = 4800
 
 # Define file to compute embeddings for
-# file = 'LectureTable'
-file = 'QATable'
-file_path = './data/' + file + '.txt'
+file = 'T1_Q_Sentence'
+file_path = './data/' + file + '.csv'
 
 # load file
 with open(file_path, 'r') as f:
-  file_list = list(unicodecsv.reader(f, delimiter='\t'))
-# get list of speeches
-speeches = []
-for entry in file_list:
-    speeches.append(entry[8])
+  datastore = list(unicodecsv.reader(f, delimiter=','))
 
-sentences = []
-# for each speech, tokenize into list of speech_sentences, extend sentences
-for speech in speeches:
-    # speech.encode(encoding='UTF-8',errors='replace')
-
-    speech_sentences = tokenize.sent_tokenize(speech)
-    sentences.extend(speech_sentences)
-
-
-# Convert array of sentences vectors into array of vectors
-print('encoding...')
+sentences = [x[1] for x in datastore]
+vectors = np.empty((0, embedding_size ))
+print('Encoding...')
 
 batch_size = 4000
 num_batches = len(sentences) / batch_size
+output_file_index = 0
+checkpoint_frequency = 20
 
 for batch_number in tqdm(range(num_batches)):
+  # checkpoint save every 10% progress or so
     batch_start_index = batch_number * batch_size
-    if batch_number == num_batches:
+    if batch_number == num_batches - 1:
         batch_end_index = len(sentences)
     else:
         batch_end_index = batch_start_index + batch_size
-
+        
     batch_sentences = sentences[batch_start_index:batch_end_index]
     batch_vectors = encoder.encode(batch_sentences, verbose=False)
-    numpy.savetxt("./results/" + file + "_embeddings_batch_"+ str(batch_number) +  ".csv", batch_vectors, delimiter=",")
+    vectors = np.vstack([vectors, batch_vectors])
+    if (batch_number + 1) % checkpoint_frequency) == 0:
+      np.save('qatable_sentences_embeddings_' + str(batch_size * checkpoint_frequency * output_file_index) + '_to_' + str(batch_size * checkpoint_frequency * (output_file_index + 1)), vectors)
+      vectors = np.empty((0, embedding_size ))
+      output_file_index += 1
+    if batch_number == num_batches - 1:
+      np.save('qatable_sentences_embeddings_' + str(batch_size * checkpoint_frequency * output_file_index) + '_to_' + str(batch_end_index), vectors)
+
+ 
